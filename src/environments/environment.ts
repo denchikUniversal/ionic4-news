4ec63a3a43600fe40ec483bee6af69d8c73b09cb
denchikUniversal <tkachenkodenys94@gmail.com>
2018-09-14 11:19:35 +0300

    http request in news service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://newsapi.org/v2/',
+  apiKey: '4370ceb1a6fe4e1ba693a36ce793609b'
 };
 
 /*
